56d9147b220700ce2b6d88bd4210dff3310c13cb
YitzhaqM <yishaktofik@gmail.com>
2018-01-12 14:38:19 -0500

    a pinch of Yishak

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/ytm2102', methods=['GET', 'POST'])
 def main():
     return render_template('index.html')
 
+@app.route('/index.html', methods=['GET', 'POST'])
+def in1():
+    return render_template('index.html')
+@app.route('/index2.html', methods=['GET', 'POST'])
+def in2():
+    return render_template('index2.html')
 
 if __name__ == '__main__':
     app.run()
